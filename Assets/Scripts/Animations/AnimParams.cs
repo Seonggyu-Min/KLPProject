628289d9d19e972dc2c152cbf242e0fbf01246ce
Seonggyu-Min <minsunggu705@gmail.com>
2025-12-18 12:41:13 +0900

    feat: 애니메이션 해싱 키 목록 추가

---
+++ Assets/Scripts/Animations/AnimParams.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnimParams
+{
+    public static readonly int NPC_IDLE = Animator.StringToHash("Idle");
+    public static readonly int NPC_PUNCHING = Animator.StringToHash("Punching");
+    public static readonly int NPC_YAWN = Animator.StringToHash("Yawn");
+}
