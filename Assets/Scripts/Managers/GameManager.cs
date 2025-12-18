011cce7ae240501e33e16c33d9006f0b0dafa4ba
Seonggyu-Min <minsunggu705@gmail.com>
2025-12-18 12:41:33 +0900

    feat: 게임매니저 추가
    - 게임 경과 시간 및 재시작 기능 지원

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private float _elapsedTime = 0f;
+    public float ElapsedTime => _elapsedTime;
+
+    [SerializeField] private float _gameOverTime = 20f;
+    [SerializeField] private GameObject _npc;
+
+    private bool _isGameOver = false;
+    public event Action OnGameOver;
+
+
+    private void Awake()
+    {
+        SingletonInit();
+    }
+
+    private void Start()
+    {
+        ResetGame();
+    }
+
+    private void Update()
+    {
+        if (_elapsedTime >= _gameOverTime)
+        {
+            if (!_isGameOver)
+            {
+                _isGameOver = true;
+                SetActiveNPC(false);
+                OnGameOver?.Invoke();
+            }
+        }
+        else
+        {
+            _elapsedTime += Time.deltaTime;
+        }
+    }
+
+
+    public void ResetGame()
+    {
+        _elapsedTime = 0f;
+        _isGameOver = false;
+        SetActiveNPC(true);
+    }
+
+
+    private void SetActiveNPC(bool active) => _npc.SetActive(active);
+}
